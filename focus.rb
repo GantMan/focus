c8436110ef4f401881aa14875c2b166159bd6335
Gant <GantMan@gmail.com>
2013-06-29 17:01:28 -0500

    Adding long description to set

--- focus.rb
+++ focus.rb
@@ -1,12 +1,25 @@
 require "bundler"
 require "thor"
+require "pathname"
+require "fileutils"
 Bundler.require
 
 class Focus < Thor
 
-	desc "set", "sets the current working directory as the latest in directory in your focus group"
+	desc "set", "sets the current directory as the latest in directory in your focus group"
+	long_desc <<-LONGDESC
+    `focus set` will add the present working directory as the latest directory and primary focus path.
+ 
+    You can optionally specify any directory other than the current using the `dir` option.
+    \x5> $ focus set
+    \x5> $ focus set --dir=~/Projects
+  LONGDESC
+	option :dir
 	def set
-		puts "I worked!"
+		# get the present working directory (requested)
+		pwd = options[:dir] || `pwd`
+		puts "pwd gave back #{pwd}"
+
 	end
 end
 
